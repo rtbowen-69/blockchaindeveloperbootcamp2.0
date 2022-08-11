95c9d269ef7c8258a03f1dfab8e09ab710b333d0
rtbowen <rtbowen@outlook.com>
2022-08-11 13:33:34 -0600

    4. Token Balances

--- test/Token.js
+++ test/Token.js
@@ -7,11 +7,15 @@ const tokens = (n) => {
 
 
 describe('Token', () => {
-	let token
+	let token, 
+			accounts, 	
+			deployer
 
 	beforeEach(async () => {
 		const Token = await ethers.getContractFactory('Token')
-		token = await Token.deploy('Dapp University', 'RODD', '1000000')	
+		token = await Token.deploy('Dapp University', 'RODD', '1000000')
+		accounts = await ethers.getSigners()
+		deployer = accounts[0]	
 	})
 
 	describe('Deployment', () => {
@@ -37,5 +41,8 @@ describe('Token', () => {
 			expect(await token.totalSupply()).to.equal(totalSupply)
 		})
 
+		it('assigns total supply to deployer', async () => {
+			expect(await token.balanceOf(deployer.address)).to.equal(totalSupply)
+		})
 	})
 }) 
