f6f2d1508d599aba61f9fc07340fc670ef5e70c3
rtbowen <rtbowen@outlook.com>
2022-08-12 16:38:50 -0600

    7. Token Tranfer Delegation

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -35,22 +35,37 @@ contract Token {
         balanceOf[msg.sender] = totalSupply;
     }
 
-    function transfer(address _to, uint256 _value)
+    function transfer(
+    	address _to, 
+    	uint256 _value
+    )
         public
         returns (bool success)
     {
         require(balanceOf[msg.sender] >= _value);
-        require(_to != address(0));
 
-        balanceOf[msg.sender] = balanceOf[msg.sender] - _value;
-        balanceOf[_to] = balanceOf[_to] + _value;
-
-        emit Transfer(msg.sender, _to, _value);
+        _transfer(msg.sender, _to, _value);
 
         return true;
     }
 
-    function approve(address _spender, uint256 _value)
+    function _transfer(
+    	address _from,
+    	address _to,
+    	uint256 _value
+    )	internal {
+    		require(_to != address(0));
+
+    		balanceOf[_from] = balanceOf[_from] - _value;
+        balanceOf[_to] = balanceOf[_to] + _value;
+
+        emit Transfer(_from	, _to, _value);
+    }
+
+    function approve(
+    	address _spender, 
+    	uint256 _value
+    )
         public
         returns(bool success)
     {
@@ -62,7 +77,22 @@ contract Token {
         return true;
     }
 
+		function transferFrom(
+			address _from, 
+			address _to, 
+			uint256 _value
+		)
+        public
+        returns(bool success)
+    {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+
+        allowance[_from][msg.sender] = allowance[_from][msg.sender] -_value;
+
+        _transfer(_from, _to, _value);
+
+        return true;
+    }
+
 }
-
-
-
