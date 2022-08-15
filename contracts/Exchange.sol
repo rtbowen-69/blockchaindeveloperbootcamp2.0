c5a099f0cceab9239a83db5c1ad125d62fa57a39
rtbowen <rtbowen@outlook.com>
2022-08-15 10:14:41 -0600

    8. Create Exchange

---
+++ contracts/Exchange.sol
@@ -0,0 +1,23 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract Exchange {
+	address public feeAccount;
+	uint256	public	feePercent;
+
+	constructor(address _feeAccount, uint256 _feePercent) {
+		feeAccount = _feeAccount;
+		feePercent = _feePercent;
+	}
+}
+	// Deposit Tokens
+	// Withdraw Tokens
+	// Check Balances
+	// Make Orders
+	// Cancel Orders
+	// Fill Orders
+	// Charge Fees
+	// Track Fee Account
+
