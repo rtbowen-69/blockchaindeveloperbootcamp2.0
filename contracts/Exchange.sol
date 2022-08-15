84df034b9295c19639e7a7d40ef7f33d896ab64d
rtbowen <rtbowen@outlook.com>
2022-08-15 14:40:06 -0600

    9. Deposit Tokens

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -1,23 +1,48 @@
-//SPDX-License-Identifier: Unlicense
+//SPDX-License-Identifier: Unlicensed
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
+import	"./Token.sol";
 
 contract Exchange {
 	address public feeAccount;
 	uint256	public	feePercent;
+	mapping(address => mapping(address => uint256)) public tokens;
+
+	event Deposit(address token, address user, uint256 amount, uint256 balance);
 
 	constructor(address _feeAccount, uint256 _feePercent) {
 		feeAccount = _feeAccount;
 		feePercent = _feePercent;
 	}
-}
+
 	// Deposit Tokens
-	// Withdraw Tokens
+
+	function depositToken(address _token, uint256 _amount) public {
+		// transfer tokens to exchange
+		require(Token(_token).transferFrom(msg.sender, address(this), _amount));
+
+		// update user balance
+		tokens[_token][msg.sender] = tokens[_token][msg.sender] + _amount;
+
+		// Emit event
+		emit Deposit(_token, msg.sender, _amount, tokens[_token][msg.sender]);
+  }
 	// Check Balances
+	function balanceOf(address _token, address _user)
+			public
+			view
+			returns (uint256)
+	{
+
+	return tokens[_token][_user];
+
+	}
+
+	// Withdraw Tokens
 	// Make Orders
 	// Cancel Orders
 	// Fill Orders
 	// Charge Fees
 	// Track Fee Account
-
+}
