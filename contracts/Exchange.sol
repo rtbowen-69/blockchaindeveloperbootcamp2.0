509025254f776a45c1c554aa704487caa43e7ad8
rtbowen <rtbowen@outlook.com>
2022-08-15 19:32:49 -0600

    10 Withdraw Tokens

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -9,7 +9,18 @@ contract Exchange {
 	uint256	public	feePercent;
 	mapping(address => mapping(address => uint256)) public tokens;
 
-	event Deposit(address token, address user, uint256 amount, uint256 balance);
+	event Deposit(
+		address token, 
+		address user, 
+		uint256 amount, 
+		uint256 balance
+	);
+	event Withdraw(
+		address token, 
+		address user, 
+		uint256 amount, 
+		uint256 balance
+	);
 
 	constructor(address _feeAccount, uint256 _feePercent) {
 		feeAccount = _feeAccount;
@@ -28,6 +39,22 @@ contract Exchange {
 		// Emit event
 		emit Deposit(_token, msg.sender, _amount, tokens[_token][msg.sender]);
   }
+
+  function withdrawToken(address _token, uint256 _amount) public {
+  	// Ensure user has enough tokens to withdraw
+  	require(tokens[_token][msg.sender] >= _amount);
+
+  	// transfer tokens to user
+  	Token(_token).transfer(msg.sender, _amount);
+
+  	// Update user balance
+  	tokens[_token][msg.sender] = tokens[_token][msg.sender] - _amount;
+
+  	// Emit event
+  	emit Withdraw(_token, msg.sender, _amount, tokens[_token][msg.sender]);
+
+  }
+
 	// Check Balances
 	function balanceOf(address _token, address _user)
 			public
