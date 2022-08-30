5013fb6b1d47af788b4ac77875ee52c070c2ef52
rtbowen <rtbowen@outlook.com>
2022-08-30 14:04:20 -0600

    20. Deposits pt1

--- src/components/App.js
+++ src/components/App.js
@@ -7,11 +7,13 @@ import {
   loadNetwork,
   loadAccount,
   loadTokens,
-  loadExchange
+  loadExchange,
+  subscribeToEvents
 } from '../store/interactions';
 
 import Navbar from './Navbar'
 import Markets from './Markets'
+import Balance from './Balance'
 
 function App() {
   const dispatch = useDispatch()
@@ -40,7 +42,10 @@ function App() {
 
     // Load Exchange smart contract
     const exchangeConfig = config[chainId].exchange
-    await loadExchange(provider, exchangeConfig.address, dispatch)
+    const exchange = await loadExchange(provider, exchangeConfig.address, dispatch)
+
+    // Listen to events
+    subscribeToEvents(exchange, dispatch)
 
   }
 
@@ -58,7 +63,7 @@ function App() {
 
           <Markets />
 
-          {/* Balance */}
+          <Balance />
 
           {/* Order */}
 
