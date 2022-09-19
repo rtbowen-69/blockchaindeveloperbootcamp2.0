8ff303d51e2448103508cdb194909f45948acee5
rtbowen <rtbowen@outlook.com>
2022-09-19 16:47:14 -0600

    26. Trades

--- src/store/selectors.js
+++ src/store/selectors.js
@@ -55,6 +55,71 @@ const decorateOrder = (order, tokens) => {
   })
 }
 
+
+// ------------------------------------------------------------------------------
+// ALL FILLED ORDERS
+
+export const filledOrdersSelector = createSelector(
+  filledOrders, 
+  tokens, 
+  (orders, tokens) => {
+    if (!tokens[0] || !tokens[1]) { return }
+
+    // Filter orders by selected tokens
+    orders = orders.filter((o) => o.tokenGet === tokens[0].address || o.tokenGet === tokens[1].address)
+    orders = orders.filter((o) => o.tokenGive === tokens[0].address || o.tokenGive === tokens[1].address)
+
+    // Sort orders by time ascending for price comparison
+    orders = orders.sort((a, b) => a.timestamp - b.timestamp)
+
+    // Decorate the orders
+    orders = decorateFilledOrders(orders, tokens)
+
+     // Sort orders by time descending for price comparison
+    orders = orders.sort((a, b) => b.timestamp - a.timestamp)
+
+    return orders
+
+  }
+)
+
+const decorateFilledOrders = (orders, tokens) => {
+  // Track previous order to compair history
+  let previousOrder = orders[0]
+
+  return(
+    orders.map((order) => {
+      // decorate each individual order
+      order = decorateOrder(order, tokens)
+      order = decorateFilledOrder(order, previousOrder)
+      previousOrder = order // Update the previous order once it's decorated
+      return order
+    })
+  )
+}
+
+const decorateFilledOrder =(order, previousOrder) => {
+  return({
+    ...order,
+    tokenPriceClass: tokenPriceClass(order.tokenPrice, order.id, previousOrder )
+  })
+}
+
+const tokenPriceClass = (tokenPrice, orderId, previousOrder) => {
+  // show green price if only one order
+  if (previousOrder.id === orderId) {
+    return GREEN
+  }
+
+  // Show green price if order price is higher than previous price
+  // Show red price if order price is lower than previous price
+  if (previousOrder.tokenPrice <= tokenPrice) {
+    return GREEN //Success    
+  } else {
+    return RED // danger
+  }
+}
+
 // ------------------------------------------------------------------------------
 // ORDER BOOK
 
